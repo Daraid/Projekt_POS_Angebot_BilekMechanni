e5dd400c5ef3a943537361e9283537df2c30fdf2
Daraid <dawid73@hotmail.com>
2016-12-11 12:11:22 +0100

    Add project files.

---
+++ Projekt_POS_BilekMechanni/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_POS_BilekMechanni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
